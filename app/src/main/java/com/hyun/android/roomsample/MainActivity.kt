2e02f127fa617542353b30c09e5c18e44d02cfef
LeeJongHyun <leejh8848@hsociety.co.kr>
2019-03-25 18:14:51 +0900

    first commit

---
+++ app/src/main/java/com/hyun/android/roomsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.android.roomsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
